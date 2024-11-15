babee4330a0ba42d7e26d30d70298a89ec2bd801
oqzak92 <oqzak92@gmail.com>
2024-11-15 11:34:55 +0100

    salut

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,11 +1,49 @@
 class Person {
-  name = "";
+  #name = "";
 
   constructor(name) {
-    this.name = name;
+    this.#name = name;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  // set name(value) {
+  //   if (this.#name.length > 10) {
+  //     console.log("non");
+  //     return;
+  //   }
+  //   this.#name = value;
+  // }
+
+  direBonjour() {
+    return `Bonjour, je m'appelle ${this.#name}`;
+  }
+}
+
+class Professor extends Person {
+  #teaches = "";
+
+  constructor(name, subject) {
+    super(name);
+    this.#teaches = subject;
   }
 
   direBonjour() {
-    return `Bonjour, je m'appelle ${this.name}`;
+    return super.direBonjour() + " et j'enseigne " + this.#teaches;
+  }
+}
+
+class Student extends Person {
+  #year = "";
+
+  constructor(name, year) {
+    super(name);
+    this.#year = year;
+  }
+
+  direBonjour() {
+    return super.direBonjour() + " et je suis en année " + this.#year;
   }
 }

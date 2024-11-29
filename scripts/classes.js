9f535add01580d3ccb78fdf98f2e838979810cdc
oqzak92 <oqzak92@gmail.com>
2024-11-29 17:01:53 +0100

    Ajout de la gestion des salles et des professeurs

--- scripts/classes.js
+++ scripts/classes.js
@@ -30,10 +30,15 @@ class Professor extends Person {
     this.#teaches = subject;
   }
 
+  get teaches() {
+    return this.#teaches;
+  }
+
   direBonjour() {
     return super.direBonjour() + " et j'enseigne " + this.#teaches;
   }
 }
+
 
 class Student extends Person {
   #year = "";
@@ -43,7 +48,55 @@ class Student extends Person {
     this.#year = year;
   }
 
+  get year() {
+    return this.#year;
+  }
+
   direBonjour() {
     return super.direBonjour() + " et je suis en année " + this.#year;
   }
 }
+
+
+
+
+class Salle {
+  #nom = "";
+  #professor = null;
+  #students = [];
+
+  constructor(nom) {
+    this.#nom = nom;
+  }
+
+  get nom() {
+    return this.#nom;
+  }
+
+  get professor() {
+    return this.#professor;
+  }
+
+  set professor(prof) {
+    if (prof instanceof Professor) {
+      this.#professor = prof;
+    } else {
+      throw new Error("Le professeur doit être une instance de la classe Professor.");
+    }
+  }
+
+  get students() {
+    return this.#students;
+  }
+
+  addStudent(student) {
+    if (student instanceof Student) {
+      this.#students.push(student);
+    } else {
+      throw new Error("L'étudiant doit être une instance de la classe Student.");
+    }
+  }
+}
+
+
+
